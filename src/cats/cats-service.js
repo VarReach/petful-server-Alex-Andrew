6d687a132317aaa1d357f104aae875d9ad5ba063
andrewbituin <fandrewbituin@gmail.com>
2019-04-12 19:03:42 -0700

    working on debugging

--- src/cats/cats-service.js
+++ src/cats/cats-service.js
@@ -1,14 +1,11 @@
 const { cats } = require('./store-cats');
 
-//normally you would store these in a DB, and just pull the 5 most recent. This attempts to emulate that.
-const ADOPTED_CATS_LIMIT = 5;
-
 const catsService = {
   addAdoptedCat(name, adoptedBy) {
     console.log(name, adoptedBy);
     const newCat = cats.find(item => item.name === name);
     newCat.adoptedBy = adoptedBy;
-  },
+  }
 };
 
 module.exports = catsService;
--- src/dogs/dogs-service.js
+++ src/dogs/dogs-service.js
@@ -1,8 +1,5 @@
 const { dogs } = require('./store-dogs');
 
-//normally you would store these in a DB, and just pull the 5 most recent. This attempts to emulate that.
-const ADOPTED_DOGS_LIMIT = 5;
-
 const dogsService = {
   addAdoptedDog(name, adoptedBy) {
     console.log(name, adoptedBy);
