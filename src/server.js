d72aa15f5b13fe920d64de2be0a6ae1ae4688ac9
AReach <DerAReich@gmail.com>
2019-04-12 21:57:30 -0500

    testing

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const app = express();
 
 const morganSetting = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
+console.log(CLIENT_ORIGIN);
 app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors({ 
